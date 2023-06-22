2b0de360e1d9373f8b4ebbd05c61b60b66aed673
GP <qkrwur724@naver.com>
2023-06-22 16:22:05 +0900

    rl

--- tpspam/spam/views.py
+++ tpspam/spam/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("spam 전화번호")
+    """
+    전화번호 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list' : question_list}
+    return render(request, 'spam/question_list.html', context)
+
+def detail(request, question_id):
+
+    """
+    발신자 정보 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'spam/question_detail.html', context)
