9889c0cfaec6a744cd9ad26ec98b3eee6fc04f54
GP <qkrwur724@naver.com>
2023-06-23 10:30:39 +0900

    rl

--- tpspam/spam/views.py
+++ tpspam/spam/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
+from django.utils import timezone
+
+from .forms import QuestionForm, AnswerForm
+
+def answer_create(request, question_id):
+    """
+    spam 댓글 등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('spam:detail', question_id=question.id)
 
 def index(request):
     """
@@ -11,10 +21,25 @@ def index(request):
     return render(request, 'spam/question_list.html', context)
 
 def detail(request, question_id):
-
     """
     발신자 정보 출력
     """
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'spam/question_detail.html', context)
+
+def question_create(request):
+    """
+    번호 등록
+    """
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('spam:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'spam/question_form.html', context)
